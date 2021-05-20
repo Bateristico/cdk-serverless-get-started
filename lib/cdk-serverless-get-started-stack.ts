ac4e8694c33efd0684fe257b6c357ad25d653620
Bateristico <camilo.saa@gmail.com>
2021-05-20 22:55:04 +0200

    auth api key

--- lib/cdk-serverless-get-started-stack.ts
+++ lib/cdk-serverless-get-started-stack.ts
@@ -1,14 +1,14 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
-import * as apigw from '@aws-cdk/aws-apigateway';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class CdkServerlessGetStartedStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
   
     // dynamodb table definition
-    const table = new dynamodb.Table(this, 'Hello', {
+    const table = new dynamodb.Table(this, 'TestTable', {
       partitionKey: { name: "name", type: dynamodb.AttributeType.STRING},
     });  
 
@@ -26,10 +26,25 @@ export class CdkServerlessGetStartedStack extends cdk.Stack {
     table.grantReadWriteData(dynamoLambda);
 
     // create the API gateway with one method and path
-    const api = new apigw.RestApi(this, "hello-api");
+    const api = new apigateway.RestApi(this, "device-token-api");
 
-    api.root.resourceForPath("Hello").addMethod("GET", new apigw.LambdaIntegration(dynamoLambda));
+    // api key logic
+    const apiKeyName = "auth-api-key"
+    const apiKey = new apigateway.ApiKey(this, `auth-api-key-id`, {
+                apiKeyName,
+                description: `APIKey used by my api to do awesome stuff`,
+                value: "THISISTHESECRETVALUE123!!",
+                enabled: true,
+            })
 
+    const usagePlanProps: apigateway.UsagePlanProps = {
+      name: "MyUsagePlan",
+      apiKey,
+    }           
+
+    api.root.resourceForPath("device-token-endpoint").addMethod("GET", new apigateway.LambdaIntegration(dynamoLambda));
+    api.addUsagePlan("MyUsagePlan", usagePlanProps)
+  
   
   }
 
